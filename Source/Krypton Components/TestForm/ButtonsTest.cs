377b3908bf9fb1a9da4542148dd46216a79b5987
Ashish Kamani <ashishkamani333@gmail.com>
2025-04-16 19:32:36 +0530

    Add project files.

---
+++ Source/Krypton Components/TestForm/ButtonsTest.cs
@@ -0,0 +1,35 @@
+﻿#region BSD License
+/*
+ * 
+ *  New BSD 3-Clause License (https://github.com/Krypton-Suite/Standard-Toolkit/blob/master/LICENSE)
+ *  Modifications by Peter Wagner(aka Wagnerp) & Simon Coghlan(aka Smurf-IV), et al. 2024 - 2025. All rights reserved. 
+ *  
+ */
+#endregion
+
+namespace TestForm
+{
+    public partial class ButtonsTest : KryptonForm
+    {
+        public ButtonsTest()
+        {
+            InitializeComponent();
+        }
+
+        private void kcbtnDropDown_SelectedColorChanged(object sender, ColorEventArgs e)
+        {
+            kryptonButton3.Values.DropDownArrowColor = e.Color;
+
+            kryptonButton4.Values.DropDownArrowColor = e.Color;
+
+            kryptonButton7.Values.DropDownArrowColor = e.Color;
+
+            kryptonButton8.Values.DropDownArrowColor = e.Color;
+        }
+
+        private void kbtnButtonStyles_Click(object sender, EventArgs e)
+        {
+            new ButtonStyleExamples().Show();
+        }
+    }
+}
